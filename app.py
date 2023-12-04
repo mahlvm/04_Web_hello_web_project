afc15be9a4765c229d14abcab3af96b9391b03c1
mahlvm <mahmoreti@hotmail.com>
2023-12-04 14:51:51 +0000

    first commit

--- app.py
+++ app.py
@@ -1,31 +1,54 @@
+# == Your Routes Here ==
+
+
+# These lines start the server if you run this file directly
+# They also start the server configured to use the test database
+# if started in test mode.
+
 import os
 from flask import Flask, request
 
 # Create a new Flask app
 app = Flask(__name__)
 
-# == Your Routes Here ==
+@app.route('/hello', methods=['GET'])
+def get_hello():
+    return 'Hello!'
 
-# == Example Code Below ==
+@app.route('/submit', methods=['POST'])
+def post_submit():
+    name = request.form['name']
+    message = request.form['message']
+    return f'Thanks {name}, yout send this message: "{message}"'
 
-# GET /emoji
-# Returns a emojiy face
-# Try it:
-#   ; curl http://127.0.0.1:5001/emoji
-@app.route('/emoji', methods=['GET'])
-def get_emoji():
-    return ":)"
+@app.route('/wave', methods=['GET'])
+def get_wave():
+    name = request.form['name']
+    return f'I am waving at {name}'
 
-# This imports some more example routes for you to see how they work
-# You can delete these lines if you don't need them.
-from example_routes import apply_example_routes
-apply_example_routes(app)
+@app.route('/count_vowels', methods=['POST'])
+def post_count_vowels():
+    text = request.form['text']
+    vowel_num = 0
+    for letter in text:
+        if letter in "aeiou":
+            vowel_num +=1
+    return f'There are {vowel_num} vowels in "{text}"'
 
-# == End Example Code ==
+@app.route('/sort-names', methods=['POST'])
+def post_sort_name():
+    names = request.form['names'].split(',')
+    sorted_names = sorted(names)
+    return ','.join(sorted_names)
 
-# These lines start the server if you run this file directly
-# They also start the server configured to use the test database
-# if started in test mode.
+@app.route('/return_names', methods=['POST'])
+def post_add_name():
+    names = request.form['names'].split(',')
+    add_name = request.form['add_name']
+    names.append(add_name)
+    return ', '.join(names)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=int(os.environ.get('PORT', 5001)))
 
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,13 +1,29 @@
-# Tests for your routes go here
-
-# === Example Code Below ===
-
-"""
-GET /emoji
-"""
-def test_get_emoji(web_client):
-    response = web_client.get("/emoji")
+def test_post_count_vowels_eee(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'eee'})
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == ":)"
+    assert response.data.decode('utf-8') == 'There are 3 vowels in "eee"'
 
-# === End Example Code ===
+def test_post_count_vowels_eunoia(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'eunoia'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 5 vowels in "eunoia"'
+
+def test_post_count_vowels_mercurial(web_client):
+    response = web_client.post('/count_vowels', data={'text': 'mercurial'})
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'There are 4 vowels in "mercurial"'
+
+def test_post_sort_names_with_list_of_names(web_client):
+    response = web_client.post('sort-names', data={
+        'names': 'Joe,Alice,Zoe,Julia,Kieran'
+    })
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Alice,Joe,Julia,Kieran,Zoe'
+
+def test_post_return_names_add(web_client):
+    response = web_client.post('return_names', data={
+        'names': 'Julia,Alice,Karim',
+        'add_name': 'Eddie'
+    })
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Julia, Alice, Karim, Eddie'
